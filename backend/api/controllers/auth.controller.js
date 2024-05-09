437e9ae7fe6e60c79b638d9e84e8b5bc61d00437
Thienngoc <thienngoc.x@gmail.gmailcom>
2024-05-09 23:16:27 +0700

    setup express.js auth tutorial

---
+++ backend/api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  //HASH THE PASSWORD
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
+
+  //CREATE A NEW USER AND SAVE DB
+};
+export const login = (req, res) => {};
+export const logout = (req, res) => {};
